b5eb62e77adb79f6123fda7c511a19cf8789d0e4
Marcelo Fronza Welter <marcelo_fw@outlook.com>
2025-05-28 11:42:56 -0300

    updating file

--- db_python.py
+++ db_python.py
@@ -1,9 +1,15 @@
+from platform import python_version
+# print(python_version())
+import sqlite3
+# print(sqlite3.sqlite_version)
 
+import pandas as pd
 
-
-
-
-
+con = sqlite3.connect("cap12_dsa.db")   # conectar no banco de dados
+cursor = con.cursor()                   # entrar no banco com o cursor para percorrer os dados no banco de dados
+sql_query = """SELECT name FROM sqlite_master WHERE type = 'table';"""
+cursor.execute(sql_query)
+print(cursor.fetchall())
 
 
 
