13a85ebedff29bf96d8c719819e04b3f43c9edf2
Marcelo Fronza Welter <marcelo_fw@outlook.com>
2025-06-03 11:29:12 -0300

    updating file

--- db_python.py
+++ db_python.py
@@ -9,14 +9,43 @@ con = sqlite3.connect("cap12_dsa.db")   # conectar no banco de dados
 cursor = con.cursor()                   # entrar no banco com o cursor para percorrer os dados no banco de dados
 sql_query = """SELECT name FROM sqlite_master WHERE type = 'table';"""
 cursor.execute(sql_query)
-print(cursor.fetchall())
+# print(cursor.fetchall())
 
 query1 = 'SELECT * FROM tb_vendas_dsa'
 cursor.execute(query1)
 nomes_colunas = [description[0] for description in cursor.description]
-print(nomes_colunas)
+# print(nomes_colunas)
 dados = cursor.fetchall()
-print(dados)
+# print(dados)
+
+query2 = 'SELECT AVG(Unidades_Vendidas) FROM tb_vendas_dsa'
+cursor.execute(query2)
+# print(cursor.fetchall())
+
+query3 = 'SELECT Nome_Produto, AVG(Unidades_Vendidas) FROM tb_vendas_dsa GROUP BY Nome_Produto'
+cursor.execute(query3)
+# print(cursor.fetchall())
+
+query4 = """SELECT Nome_Produto, AVG(Unidades_Vendidas)
+            FROM tb_vendas_dsa
+            WHERE Valor_Unitario > 199
+            GROUP BY Nome_Produto"""
+cursor.execute(query4)
+print(cursor.fetchall())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
